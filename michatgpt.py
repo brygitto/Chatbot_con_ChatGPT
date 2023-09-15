fa80662d253de093bd02f0d1b19edd0e28350042
Bryan Villamizar <103080405+brygitto@users.noreply.github.com>
2023-09-15 10:20:20 -0500

    Add files via upload

---
+++ michatgpt.py
@@ -0,0 +1,13 @@
+import openai
+
+# Indica el API KEY
+openai.api_key = "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"
+
+# Uso de ChatGPT en PYTHON
+model_engine = "text-davinci-003"
+prompt = "que comida es la mejor para los perros"
+completion = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=1024, n=1, stop=None, temperature=0.7)
+respuesta = ""
+for choice in completion.choices:
+    respuesta = respuesta + choice.text
+    print(f"Response: %s" % choice.text)
